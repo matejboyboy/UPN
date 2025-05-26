5b05d6a53d03b932b98a2573600e771ea7efbd86
matejboyboy <matej.prezelj8@gmail.com>
2025-05-27 01:15:30 +0200

    Update functions.py

--- Code/functions.py
+++ Code/functions.py
@@ -2,22 +2,6 @@ from flask import request
 import json
 import requests
 
-# def generated_words(word_count):
-#     api_url = f'https://random-word-api.herokuapp.com/word?number={word_count}'
-
-#     try:
-#         response = requests.get(api_url)
-#         response.raise_for_status()  # error ce 4xx alpa 5xx
-
-#         words = json.loads(response.text)  # json u py list
-#         if isinstance(words, list):
-#             return words
-#         else:
-#             raise ValueError("Expected a list in the JSON response.")
-#     except (requests.RequestException, json.JSONDecodeError, ValueError) as e:
-#         print("Error:", e)
-#         return []
-
 def api(amount_of_words):
     # http://127.0.0.1:8000/words?amount=5
     response = requests.get(f'http://127.0.0.1:8000/words?amount={amount_of_words}').json()
